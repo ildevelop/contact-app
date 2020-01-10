bfcb18c8209d5d882d1497f258e3819702d54029
ildevelop <raduilya@gmail.com>
2020-01-10 21:49:41 +0200

    [fix] clean code

---
+++ Procfile
@@ -0,0 +1,1 @@
+cd server && npm i && node index.js
--- src/style/main.js
+++ src/style/main.js
@@ -5,22 +5,3 @@ export const Wrapper = styled.div`
   flex-direction: ${({ direction }) => (direction ? direction : "row")};
   margin: ${({ margin }) => (margin ? margin : 0)};
 `;
-export const Title = styled.div`
-  width: 33%;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`;
-export const TableHead = styled(Wrapper)`
-  background: #9e9f9f;
-`;
-export const TableRow = styled(Wrapper)`
-  background: ${({ item }) => (item ? "#fff" : "#9e9f9f")};
-  border-bottom: 1px solid #9e9f9f;
-  cursor: ${({ item }) => (item ? "pointer" : "default")};
-  &:hover {
-    background: ${({ item }) => (item ? "#3aa9f4" : "none")};
-  }
-`;
